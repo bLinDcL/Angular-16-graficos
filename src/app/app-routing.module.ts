9bc8c27223346f0c3b863c2a552b43c1c047419f
FVergaraA <fe.vergaraa@gmail.com>
2019-10-08 16:52:47 -0300

    Gráfico de Radar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { LineaComponent } from './components/linea/linea.component';
 import { BarraComponent } from './components/barra/barra.component';
 import { DonaComponent } from './components/dona/dona.component';
+import { RadarComponent } from './components/radar/radar.component';
 
 const ROUTES: Routes = [
   { path: 'barra', component: BarraComponent },
   { path: 'linea', component: LineaComponent },
   { path: 'dona', component: DonaComponent },
+  { path: 'radar', component: RadarComponent },
   { path: '**', pathMatch: 'full', redirectTo: 'linea' }
 ];
 
