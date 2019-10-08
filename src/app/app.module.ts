c378ee01221ebe893d71170edccd5d6e1b9b5c13
FVergaraA <fe.vergaraa@gmail.com>
2019-10-08 15:23:24 -0300

    Gráficos de Líneas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 import { ChartsModule } from 'ng2-charts';
+import { LineaComponent } from './components/linea/linea.component';
+import { BarraComponent } from './components/barra/barra.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LineaComponent,
+    BarraComponent
   ],
   imports: [
     BrowserModule,
